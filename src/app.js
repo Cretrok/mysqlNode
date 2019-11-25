adc3cb857bf8b47a444f309577a3bd9007f607a6
Cretrok <solosemeocurioesto@gmail.com>
2019-11-25 17:17:27 -0600

    Mod app.js add morgan

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const path = require('path');
 
 app.set('port', process.env.PORT || 3000);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(morgan('dev'));
 
 app.listen(app.get('port'), () => {
 	console.log('Servidor Corriendo en el puerto 3000');
