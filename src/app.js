a4635a9831f2c3982935e7d5a4f6bf1269f911a8
Cretrok <solosemeocurioesto@gmail.com>
2019-11-24 21:57:26 -0600

    mod archivo ap

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-
+app.set('view engine', 'ejs');
 
 app.listen(app.get('port'), () => {
 	console.log('Servidor Corriendo en el puerto 3000');
