5fccf3e7021b0151f105245d05cc28e6d92a8b4d
Cretrok <solosemeocurioesto@gmail.com>
2019-11-25 22:30:55 -0600

    connection database in file app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
 const path = require('path');
 
 app.set('port', process.env.PORT || 3000);
@@ -9,6 +11,13 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(morgan('dev'));
+app.use(myConnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: 3306,
+    database: 'mysqlnode'
+}, 'single'));
 
 app.listen(app.get('port'), () => {
 	console.log('Servidor Corriendo en el puerto 3000');
